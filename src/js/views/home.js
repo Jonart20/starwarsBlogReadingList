374ee545e81987389cad196ead4dfb604e123978
Jonart20 <jduran542@gmail.com>
2021-04-14 07:44:45 +0000

    final

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,29 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useContext } from "react";
 import "../../styles/home.scss";
+import { CharactersCard } from "../component/charactersCard";
+import { PlanetsCard } from "../component/planetsCard";
+import { Context } from "../store/appContext";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+	const { store, actions } = useContext(Context);
+	const changeIconColor = name => {
+		const nameBelonging = store.favorites.findIndex(favorite => favorite == name);
+		if (nameBelonging === -1) {
+			return <i className="fas fa-heart" style={{ color: "yellow" }} />;
+		} else {
+			return <i className="fas fa-heart" style={{ color: "red" }} />;
+		}
+	};
+	return (
+		<div className="container mt-5">
+			<div className="row">
+				<h2>Characters</h2>
+			</div>
+			<CharactersCard icon={changeIconColor} />
+			<div className="row mt-5">
+				<h2>Planets</h2>
+			</div>
+			<PlanetsCard icon={changeIconColor} />
+		</div>
+	);
+};
